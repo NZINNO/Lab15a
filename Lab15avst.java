6ba3f5e3453d98a520ce2f81613fe777dbaf3988
Nick <nicholas.zinno@vtsd.com>
2020-03-21 23:25:01 -0400

    maybe?

--- Lab15avst.java
+++ Lab15avst.java
@@ -14,7 +14,7 @@ public class Lab15avst
 		Scanner input = new Scanner(System.in);
 
       System.out.print("Enter the odd# size of the Magic Square -->  ");
-      int size = input.nextInt();
+      int size = 3;
       MagicSquare magic = new MagicSquare(size);
 
       magic.computeMagicSquare();
@@ -31,6 +31,8 @@ class MagicSquare
 	private int size;
 	private int magic[][];
 	private DecimalFormat output;
+	private int row;
+	private int col;
 
 	public MagicSquare(int s)
 	{
@@ -41,9 +43,18 @@ class MagicSquare
 
 	public void computeMagicSquare()
 	{
+		int row = 0; int col = (size -1)/2; magic[row][col] = 1;
+		for(int i=2; i <= (size*size); i++){
+			if (magic[(row + 1) % size][(col + 1) % size] == 0) {
+			row = (row + 1) % size;
+						 col = (col + 1) % size;
+				 }
+				 else {
+						 row = (row - 1 + size) % size;
 
-
-
+				 }
+				 magic[row][col] = i;
+		}
 
 	}
 
@@ -53,6 +64,12 @@ class MagicSquare
 		System.out.println(size + "x" + size + " Magic Square");
 		System.out.println("==================");
 		System.out.println();
+		for (int x = 0; x<=size; x++){
+			for int y = 0; y<=size; y++){
+				System.out.print(magic[x][y]);
+				System.out.print("\t");
+			}
+		}
 	}
 
 	public void checkRows()
@@ -78,6 +95,5 @@ class MagicSquare
 		System.out.println("==================");
 		System.out.println();
 	}
-   
-}
 
+}
